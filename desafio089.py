c248a517d95a5689064454fe94641d409a4dd183
Nicolas De Souza <nicolassouza6636@gmail.com>
2026-04-15 17:30:36 -0300

    desafio089, Lista com alunos

---
+++ desafio089.py
@@ -0,0 +1,12 @@
+ficha = []
+
+while True:
+    nome = str(input('Nome: '))
+    nota1 = float(input('Nota 1: '))
+    nota2 = float(input('Nota 2: '))
+    media = (nota1 + nota2) / 2
+    ficha.append([nome, [nota1, nota2], media])
+    r = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if r in 'Nn':
+        break
+print (ficha)
