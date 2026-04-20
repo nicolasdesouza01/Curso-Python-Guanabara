b97c2f77b99a24d88e0a4a472bcacfb66a6d523d
Nicolas De Souza <nicolassouza6636@gmail.com>
2026-04-20 16:25:30 -0300

    desafio80, ordenando números sem sorted

--- desafio080.py
+++ desafio080.py
@@ -1,16 +1,15 @@
 lista = []
-for c in range(0, 5):
+for i in range (0, 5):
     n = int(input('Digite um valor: '))
-    if c == 0 or n > lista[-1]:
+    if i == 0 or n > lista[-1]:
         lista.append(n)
-        print ('Valor adicionado ao final da lista')
+        print ('Adicionando ao final da lista')    
     else:
         pos = 0
         while pos < len(lista):
             if n <= lista[pos]:
                 lista.insert(pos, n)
-                print (f'Valor adicionado na posição {pos} da lista')
+                print (f'Adicionando na posição {pos} da lista')
                 break
             pos += 1
-print ('-='*30)
-print (f'Os valores digitados em ordem foram {lista}')
+print(f'Os valores digitados em ordem foram {lista}')
